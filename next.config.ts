704f4f8a7fa3aa4d42391dd6bcffbd7c3f55c028
Paurakh <pandeypaurakhraj@gmail.com>
2025-11-09 01:37:02 +0545

    integrated next-pwa plugin

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
+// @ts-ignore: no type definitions for next-pwa
+import withPWAInit from "next-pwa";
+
+const withPWA = withPWAInit({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+});
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {}, 
 };
 
-export default nextConfig;
+export default withPWA(nextConfig);
